5b3d1139bdd2897d18e2be54f3427a91ada49288
armshare <armshare.it@gmail.com>
2020-04-05 02:01:23 +0700

    pratice #2

--- 01 - Falling Snow/start.js
+++ 01 - Falling Snow/start.js
@@ -1,10 +1,9 @@
 (() => {
-  // เริ่มเขียนโค้ด
-  function setup() {
+  //create canvas size == size window
+  function createCanvasArea() {
     const canvas = document.getElementById("falling-snow-canvas");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-
     return {
       canvas,
       canvasContext: canvas.getContext("2d"),
@@ -12,65 +11,59 @@
     };
   }
 
+  // utility fn eg. random()
   function random(min, max) {
     return Math.floor(Math.random() * max - min + 1) + min;
   }
-
+  //create snowball
   function createSnowballs(canvas, numberOfSnowballs) {
     return [...Array(numberOfSnowballs)].map((i) => {
       return {
         x: random(0, canvas.width),
         y: random(0, canvas.height),
-        opacity: random(0.5, 1),
         radius: random(1, 4),
-        speedX : random(-5,5),
-        speedy : random(1,3)
+        opacity: random(0.5, 1),
+        speedX: random(-5, 5),
+        speedY: random(1, 3),
       };
     });
   }
 
-  function drawSnowball(canvasContext, snowballs) {
+  //draw snowball
+  function drawSnowball(canvasContext, snowball) {
     canvasContext.beginPath();
-    canvasContext.arc(
-      snowballs.x,
-      snowballs.y,
-      snowballs.radius,
-      0,
-      Math.PI * 2
-    );
-    canvasContext.fillStyle = `rgba(255, 255, 255, ${snowballs.opacity})`;
+    canvasContext.arc(snowball.x, snowball.y, snowball.radius, 0, 2 * Math.PI);
+    canvasContext.fillStyle = `rgba(255,255,255,${snowball.opacity})`;
     canvasContext.fill();
   }
 
-  function moveSnowball(snowball, canvas) {
+  // move snowball
+  function moveSnowBall(snowball, canvas) {
     snowball.x += snowball.speedX;
-    snowball.y += snowball.speedy;
+    snowball.y += snowball.speedY;
 
-    if(snowball.x > canvas.width){
+    if (snowball.x > canvas.width) {
       snowball.x = 0;
-    }else if(snowball.x < 0 ){
+    } else if (snowball.x < 0) {
       snowball.x = canvas.width;
     }
 
-    if(snowball.y > canvas.height){
+    if( snowball.y > canvas.height){
       snowball.y = 0;
     }
   }
 
   function run() {
-    const { canvas, canvasContext, numberOfSnowballs } = setup();
+    const { canvas, canvasContext, numberOfSnowballs } = createCanvasArea();
     const snowballs = createSnowballs(canvas, numberOfSnowballs);
-    
+
     setInterval(() => {
-      canvasContext.clearRect(0,0,canvas.width, canvas.height);
-      snowballs.forEach((element) => {
-        drawSnowball(canvasContext, element);  
-       
-      });  // 
-      snowballs.forEach((element) => {
-        moveSnowball(element, canvas);
-      }); 
+      canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+      snowballs.map((snow) => drawSnowball(canvasContext, snow));
+
+      snowballs.map((snow) => moveSnowBall(snow, canvas));
     }, 50);
   }
+
   run();
 })();
