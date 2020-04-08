c99e56413b9f6014d16dd34f658b149281204736
armshare <armshare.it@gmail.com>
2020-04-09 01:27:18 +0700

    finish day 5

---
+++ public/05 - Watercolor Painting/start.js
@@ -0,0 +1,51 @@
+(() => {
+  // เริ่มเขียนโค้ด
+  const canvas = document.getElementById("painting");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  const context = canvas.getContext("2d");
+
+  let previousPoint = { x: 0, y: 0 };
+
+  function getDistance(previousPoint, currentPoint) {
+    console.log("getDistance ", previousPoint, currentPoint);
+    return Math.sqrt(
+      (previousPoint.x - currentPoint.x) ** 2 +
+        (previousPoint.y - currentPoint.y) ** 2
+    );
+  }
+
+  function onMouseEnter({ pageX, pageY }) {
+    previousPoint.x = pageX;
+    previousPoint.y = pageY;
+  }
+
+  function onMouseMove({ pageX, pageY }) {
+    const currentPoint = { x: pageX, y: pageY };
+
+    const distance = getDistance(previousPoint, currentPoint);
+
+    context.beginPath();
+    context.lineCap = "round";
+    context.lineJoin = "round";
+    context.lineWidth =  (Math.random() / distance) * 40;
+   // console.log("distance", distance);
+    const opacity = Math.min(0.5, 1 / distance);
+   // console.log("opacity", opacity);
+    context.strokeStyle = `rgba(222, 10, 09, ${opacity})`;
+
+    context.moveTo(previousPoint.x, previousPoint.y);
+    context.lineTo(currentPoint.x, currentPoint.y);
+
+    context.stroke();
+    context.closePath();
+
+    previousPoint = currentPoint;
+  }
+  function run() {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mouseenter", onMouseEnter);
+  }
+  run();
+})();
