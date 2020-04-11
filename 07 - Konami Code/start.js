5a088591bfbf272df972f9c28f6b3b8d93c4cbd0
armshare <armshare.it@gmail.com>
2020-04-11 23:21:33 +0700

    finish day07

--- 07 - Konami Code/start.js
+++ 07 - Konami Code/start.js
@@ -1,4 +1,29 @@
 (() => {
   // เริ่มเขียนโค้ด
+  const konamiCode = [
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a",
+  ];
+  let index = 0;
 
+  function onKeyDown(event) {
+    // console.log(event);
+    event.key === konamiCode[index] ? index++ : index = 0;
+    if(index === konamiCode.length){
+      startSnowing();
+    }
+  }
+
+  function run() {
+    document.addEventListener("keydown", onKeyDown);
+  }
+  run();
 })();
