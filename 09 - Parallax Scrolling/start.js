798a6776e6d4f7d787f7ea2eac25a8e570c03ab1
armshare <armshare.it@gmail.com>
2020-04-14 21:19:59 +0700

    finish day 09

--- 09 - Parallax Scrolling/start.js
+++ 09 - Parallax Scrolling/start.js
@@ -1,4 +1,16 @@
 (() => {
   // เริ่มเขียนโค้ด
+function onscroll(){
+  const moon =  document.querySelector('.moon');
+  const label = document.querySelector('.wish');
 
+  moon.style.transform = `translate(${window.scrollY * 0.7}%  , ${window.scrollY * -0.7}%)`;
+  label.style.transform = `translateY( ${window.scrollY * -1.7}%)`
+}
+
+
+  function run(){
+    document.addEventListener('scroll', onscroll);
+  }
+  run();
 })();
