04ef19437fd573537aab7d929ed3a95f37ebd5d2
armshare <armshare.it@gmail.com>
2020-04-16 23:41:32 +0700

    finish day 09

--- 10 - Kanban Board/start.js
+++ 10 - Kanban Board/start.js
@@ -1,4 +1,34 @@
 (() => {
   // เริ่มเขียนโค้ด
+  let dragElem; 
+  function onDrag() {
+    dragElem = this;
+  }
 
+  function onDrop() {
+    this.appendChild(dragElem);
+    dragElem  = null;
+  }
+
+  function onDragOver(e) {
+    e.preventDefault();
+  }
+  function onDragEnter(e) {
+    e.preventDefault();
+  }
+  function run() {
+    const dragElems = Array.from(document.querySelectorAll(".task"));
+    const dropzoneElems = Array.from(document.querySelectorAll(".drop-zone"));
+
+    dragElems.forEach((dragElem) => {
+      dragElem.addEventListener("dragstart", onDrag);
+    });
+
+    dropzoneElems.forEach((dropzone) => {
+      dropzone.addEventListener("drop", onDrop);
+      dropzone.addEventListener("dragover", onDragOver);
+      dropzone.addEventListener("dragenter", onDragEnter);
+    });
+  }
+  run();
 })();
