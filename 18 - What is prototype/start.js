760e2c8a124fc2f2e6cb1ea8f2e181fb9859b339
armshare <armshare.it@gmail.com>
2020-05-04 10:49:57 +0700

    fininsh day 18

--- 18 - What is prototype/start.js
+++ 18 - What is prototype/start.js
@@ -1,8 +1,27 @@
 (() => {
   // เริ่มเขียนโค้ด
-
+  
   // 1. Class vs Prototype 
-  // 2. What's prototype?
+  // class Person{
+
+  // }
+  // const newPerson = new Person();
+  //   console.log(newPerson);
+  //   console.log(newPerson.__proto__);
+  // // 2. What's prototype?
+  // const name = 'Teerawut';
+  // console.log(name.__proto__);
   // 3. Prototype chain
+  // const name = 'Teerawut';
+  // console.log(name.__proto__);  // no have toLocaleString();
+  // console.log(name.toLocaleString()); // toLocaleString() from Class Object 
+
   // 4. Extend a prototype
+  const name = 'Teerawut';
+  function sayHello(val){
+    console.log(`Hello ${val}`);
+  }
+
+  String.prototype.sayHello = sayHello;
+  name.sayHello('World');
 })();
